6f0a208f5cc06c5c119ad061510a1ecd66416d13
liberty8080 <zhaojiawei233@gmail.com>
2021-04-30 21:58:53 +0800

    改host

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ oldImage=`docker images -aq ${imageName}`
 if [ -n ${oldContainer} ]; then docker rm -f ${containerName}; fi
 if [ -n ${oldImage} ];  then docker rmi ${oldImage}; fi
 docker build . -t ${imageName}
-docker run -d -p 8081:8080 --name jacob-assistant ${imageName} --restart=always --network host
+docker run -d --name jacob-assistant ${imageName} --restart=always --network host
