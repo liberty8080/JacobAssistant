0f531386aaa0a7bd0a83a7becdba156b00a725a7
liberty8080 <zhaojiawei233@gmail.com>
2021-04-30 21:07:32 +0800

    debug wake on lan

--- JacobAssistant/Commands/SimpleCommands.cs
+++ JacobAssistant/Commands/SimpleCommands.cs
@@ -20,29 +20,30 @@ namespace JacobAssistant.Commands
         {
             _provider = provider;
         }
-        [Cmd("help", Order = 0,Desc = "帮助")]
+
+        [Cmd("help", Order = 0, Desc = "帮助")]
         public string Help(MessageEventArgs e, params string[] args)
         {
             var commands = AssistantBotClient.GetCommands().ToList()
                 .OrderBy(info => info.GetCustomAttribute<Cmd>()?.Order);
             return commands.Select(methodInfo => methodInfo.GetCustomAttribute<Cmd>())
                 .Where(attr => attr != null)
-                .Aggregate("", (current, attr) 
+                .Aggregate("", (current, attr)
                     => current + $"/{attr.Name}: {attr.Desc}\n");
         }
 
-        [Cmd("ip", Order = 1,Desc = "查询当前公网ip")]
+        [Cmd("ip", Order = 1, Desc = "查询当前公网ip")]
         public string Ip(MessageEventArgs e, params string[] args)
         {
             return IpService.GetPublicIp();
         }
 
-        [Cmd("ddns",Order = 2,Desc = "同步DDNS")]
+        [Cmd("ddns", Order = 2, Desc = "同步DDNS")]
         // ReSharper disable once InconsistentNaming
         public string DDNS(MessageEventArgs e, params string[] args)
         {
             var configService = new ConfigService(new AssistantDbContext(), false);
-            return IpService.Ddns(configService.Username,configService.Password,configService.HostName);
+            return IpService.Ddns(configService.Username, configService.Password, configService.HostName);
         }
 
         [Cmd("expire", Order = 3, Desc = "过期时间查询")]
@@ -54,22 +55,19 @@ namespace JacobAssistant.Commands
         [Cmd("wake", Order = 4, Desc = "wake on lan")]
         public string WakeOnLan(MessageEventArgs e, params string[] args)
         {
+            string ip;
+            ip = args.Length > 0 ? args[1] : "255.255.255.255";
             var configService = new ConfigService(new AssistantDbContext(), false);
-
-            if (WakeOnLanService.WakeUp(configService.TargetMac,2304,"192.168.98.255") == 102)
-            {
-                return "魔术包已发出";
-            }
-            return "发送失败！";
+            return WakeOnLanService.WakeUp(configService.TargetMac, 2304, ip) == 102 ? "魔术包已发出" : "发送失败！";
         }
+
         private string V2SubLink()
         {
             using var scope = _provider.CreateScope();
             var configService = scope.ServiceProvider.GetService<ConfigService>();
-            if(configService!=null)
+            if (configService != null)
                 return configService.V2SubLink();
             throw new Exception("获取v2连接失败!");
         }
-        
     }
 }
