df4de97b44686fc5bb095af56586b05a0240f482
liberty8080 <zhaojiawei233@gmail.com>
2021-04-30 09:16:08 +0800

    rmi test

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ containerName="jacob-assistant";imageName="jacob-assistant:latest"
 oldContainer=`docker ps -aq -f name=${containerName}`
 oldImage=`docker images -aq ${imageName}`
 if [ -n ${oldContainer} ]; then docker rm -f ${containerName}; fi
-# if [ -n ${oldImage} ];  then docker rmi ${oldImage}; fi
+if [ -n ${oldImage} ];  then docker rmi ${oldImage}; fi
 docker build . -t ${imageName}
 docker run -d -p 8081:8080 --name jacob-assistant ${imageName} --restart=always
