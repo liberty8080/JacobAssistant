ed60d48e3c8bfb9eecdc054840f1a79d2ea85b84
liberty8080 <zhaojiawei233@gmail.com>
2021-07-20 09:24:58 +0800

    fixed swagger 404

--- JacobAssistant.Server/Startup.cs
+++ JacobAssistant.Server/Startup.cs
@@ -66,7 +66,7 @@ namespace JacobAssistant
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
